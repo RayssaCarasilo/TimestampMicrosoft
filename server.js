592ba5879f5a56eed93ee7dc3790675b294f8794
RayssaCarasilo <124007497+RayssaCarasilo@users.noreply.github.com>
2025-04-05 18:10:13 +0000

    05-04-25

--- server.js
+++ server.js
@@ -29,4 +29,9 @@ app.get("/api/hello", function (req, res) {
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+  if (err) {
+    console.error('Erro ao iniciar o servidor:', err);
+  } else {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  }
 });
